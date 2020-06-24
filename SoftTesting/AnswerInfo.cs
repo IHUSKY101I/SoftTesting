2ea6aa3387522f621225561f68b40edec79903df
NikolayDi <67045564+NikolayDi@users.noreply.github.com>
2020-06-24 17:09:37 +0300

    Add Answerinfo

---
+++ SoftTesting/AnswerInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftTesting
+{
+    class AnswerInfo
+    {
+        private TestType testType;
+        private string singleAnswer;
+        private string[] multyAnswer;
+
+        public AnswerInfo(TestType testType, string singleAnswer, string[] multyAnswer)
+        {
+            this.testType = testType;
+            this.singleAnswer = singleAnswer;
+            this.multyAnswer = multyAnswer;
+        }
+
+
+
+        public TestType TestType
+        {
+            get
+            {
+                return testType;
+            }
+        }
+        public string SingleAnswer
+        {
+            get
+            {
+                return singleAnswer;
+            }
+        }
+        public string[] MultyAnswer
+        {
+            get
+            {
+                return multyAnswer;
+            }
+        }
+    }
+}
